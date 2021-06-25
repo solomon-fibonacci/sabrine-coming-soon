9a0c24f62d6bf1b5728c5c05496411e9e4c8d9c9
Solomon Omojola <omojolasolomon@gmail.com>
2021-06-25 08:09:12 +0100

    add jq

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,23 @@
     }
     gtag('js', new Date())
     gtag('config', 'G-GRR8YK0SD9')
+    ;(function ($) {
+        window.fnames = new Array()
+        window.ftypes = new Array()
+        fnames[0] = 'EMAIL'
+        ftypes[0] = 'email'
+        fnames[1] = 'FNAME'
+        ftypes[1] = 'text'
+        fnames[2] = 'LNAME'
+        ftypes[2] = 'text'
+        fnames[3] = 'ADDRESS'
+        ftypes[3] = 'address'
+        fnames[4] = 'PHONE'
+        ftypes[4] = 'phone'
+        fnames[5] = 'BIRTHDAY'
+        ftypes[5] = 'birthday'
+    })(jQuery)
+    var $mcj = jQuery.noConflict(true)
 
     var $body = document.querySelector('body')
 
